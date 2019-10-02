3d8cf1995f0e82058036f1e4499018e1344adf34
Marek <marekpastierik15@gmail.com>
2019-10-02 10:50:14 +0200

    MODULE statistics: enable more than one database

--- manifests/role/server.pp
+++ manifests/role/server.pp
@@ -1,22 +1,21 @@
 class statistics::role::server
 {
-  $database = $::statistics::database
+  $databases = $::statistics::databases
 
-  package { $::statistics::server_packages :
+  package { $::statistics::server_packages:
     ensure => "present",
   }
   
-  package { 'database for grafana':
-   name   => $database,
+  package { $databases:
    ensure => "present",
   }
 
   $_data_for_template = { 
-                          "database"               => $database,
-                          "influxdb_port"          => $::statistics::influx_port,
-                          "listen_port"            => $::statistics::collectd_listen_port,
-                          "collectd_exporter_port" => $::statistics::collectd_exp_port,
-                          "dir"                    => $::statistics::path_to_plugins,
+                          "databases"               => $databases,
+                          "influxdb_port"           => $::statistics::influx_port,
+                          "listen_port"             => $::statistics::collectd_listen_port,
+                          "collectd_exporter_port"  => $::statistics::collectd_exp_port,
+                          "dir"                     => $::statistics::path_to_plugins,
                         }
 
   file { 'collectd_config':
@@ -33,7 +32,7 @@ class statistics::role::server
     content => "${::statistics::collectd_username}: ${statistics::collectd_password}",
     require => Package['collectd'],  
   }
-
+  
   $::statistics::plugins.each |$plugin| {
     if $plugin =~ Hash {
          $name     = keys($plugin)[0]
@@ -48,72 +47,74 @@ class statistics::role::server
      }
   }
 
-  if $::statistics::database == "prometheus2" {
-      
-      $flags_for_service = "--storage.tsdb.path ${::statistics::prometheus_storage} --storage.tsdb.retention.time ${::statistics::prometheus_retention_time} --web.listen-address ${::statistics::prometheus_listen_address}"
- 
-      package { 'collectd_exporter':
-        ensure  => "present",
-      }
+  unique($databases).each |$database| { # filter duplicities
 
-      service { 'collectd_exporter':
-        enable  => true,
-        ensure  => 'running',
-        require => Package['collectd_exporter'],
-      }
+      if $database == "prometheus2" {
+          
+          $flags_for_service = "--storage.tsdb.path ${::statistics::prometheus_storage} --storage.tsdb.retention.time ${::statistics::prometheus_retention_time} --web.listen-address ${::statistics::prometheus_listen_address}"
      
-      file { 'config for prometheus2':
-        ensure  => 'present',
-        path    => '/etc/prometheus/prometheus.yml',
-        content => epp('statistics/prometheus_config.epp'),
-        require => Package['database for grafana'],  
+          package { 'collectd_exporter':
+            ensure  => "present",
+          }
+    
+          service { 'collectd_exporter':
+            enable  => true,
+            ensure  => 'running',
+            require => Package['collectd_exporter'],
+          }
+         
+          file { 'config for prometheus2':
+            ensure  => 'present',
+            path    => '/etc/prometheus/prometheus.yml',
+            content => epp('statistics/prometheus_config.epp'),
+            require => Package[$database],  
+          }
+     
+          $storage = $::statistics::prometheus_storage
+    
+          file { $storage:
+            ensure => directory,
+            group  => "prometheus",
+            owner  => "prometheus",
+            mode   => "0755",
+          }
+          
+          service { "prometheus":
+            enable  => true,
+            ensure  => 'running',
+            flags   => $flags_for_service,
+            require => [ File[$storage], File["config for ${database}"] ],
+          }
+    
+      } elsif $database == "influxdb" {
+          
+          $flags_for_service = ""
+          $storage = $::statistics::influx_storage
+          
+          file { 'config for influxdb':
+            ensure  => 'present',
+            path    => '/etc/influxdb/influxdb.conf',
+            content => epp('statistics/influxdb_config.epp', { "storage" => $storage, "collectd_port" => $::statistics::influx_port, "database_name" => $::statistics::influx_database_name, "bind_address" => $::statistics::influx_bind_address }),
+            require => Package[$database],
+          }
+    
+          file{ $storage:
+            ensure  =>  directory,
+            group   => "influxdb",
+            owner   => "influxdb",
+            mode    =>  "0755",
+          }
+    
+          service { "influxdb":
+            enable  => true,
+            ensure  => 'running',
+            flags   => $flags_for_service,
+            require => [ File[$storage], File["config for ${database}"] ],
+          }
+    
+      } else {
+          fail("Use only influxdb or prometheus2 as database")
       }
- 
-      $storage = $::statistics::prometheus_storage
-
-      file { $storage:
-        ensure => directory,
-        group  => "prometheus",
-        owner  => "prometheus",
-        mode   => "0755",
-      }
-      
-      service { "prometheus":
-        enable  => true,
-        ensure  => 'running',
-        flags   => $flags_for_service,
-        require => [ File[$storage], File["config for ${database}"] ],
-      }
-
-
-  } elsif $::statistics::database == "influxdb" {
-      
-      $flags_for_service = ""
-      $storage = $::statistics::influx_storage
-      
-      file { 'config for influxdb':
-        ensure  => 'present',
-        path    => '/etc/influxdb/influxdb.conf',
-        content => epp('statistics/influxdb_config.epp', { "storage" => $storage, "collectd_port" => $::statistics::influx_port, "database_name" => $::statistics::influx_database_name, "bind_address" => $::statistics::influx_bind_address }),
-        require => Package['database for grafana'],
-      }
-
-      file{ $storage:
-        ensure  =>  directory,
-        group   => "influxdb",
-        owner   => "influxdb",
-        mode    =>  "0755",
-      }
-
-      service { "influxdb":
-        enable  => true,
-        ensure  => 'running',
-        flags   => $flags_for_service,
-        require => [ File[$storage], File["config for ${database}"] ],
-      }
-
-  } else {
-      fail("Use only influxdb or prometheus2 as database")
   }
   
   service { 'collectd':
