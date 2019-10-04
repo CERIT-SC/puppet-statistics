8c8f8d54c585d76de824edfb1b27bf7f96e52e36
Marek <marekpastierik15@gmail.com>
2019-10-04 14:59:19 +0200

    possibility to specify interval in plugin

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,9 +1,10 @@
 define statistics::plugin (
-   Hash $settings = {},
+   Hash    $settings = {},
+   Integer $interval = 300,
 ) {
    file { "${statistics::path_to_plugins}/${title}.conf":
      ensure  => 'present',
-     content => epp('statistics/plugin.epp', { "name" => $title, "options" => $settings }),
+     content => epp('statistics/plugin.epp', { "name" => $title, "options" => $settings, "interval" => $interval }),
      require => Package['collectd'],
      notify  => Service['collectd'],
    } 
