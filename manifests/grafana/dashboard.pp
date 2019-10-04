9789fa0cdf4cfd4c9fe2ca432fbcb9f9c71b615a
Marek <marekpastierik15@gmail.com>
2019-10-04 20:16:46 +0200

    specify legend for panel

--- manifests/grafana/dashboard.pp
+++ manifests/grafana/dashboard.pp
@@ -8,13 +8,29 @@ define statistics::grafana::dashboard (
    $attribute_panels = $panels.reduce([]) |$memo, $value| {
         $nameOfPanel = $value[0]
         $datasource  = $value[1]['datasource']
-        $type        = $value[1]['type']
-
-        $queries = $value[1]['queries'].reduce([]) |$accu, $query| {
-            $accu + [{ "expr" => "${query}" }]
+        
+        if has_key($value[1], 'type') {
+            $type = $value[1]['type']
+        } else {
+            $type = "graph"
         }
 
-        $memo + [{ "id" => undef, "datasource" => $datasource, "targets" => $queries, "title" => $nameOfPanel, "type" => $type }]
+        if has_key($value[1], 'legend') {
+            $legend = $value[1]['legend']
+        } else {
+            $legend = {}
+        }
+        
+        $queries = $value[1]['queries'].reduce([]) |$accu, $query| {
+            if $query =~ Hash {
+                $name_of_query = keys($query)[0]
+                $accu + [{ "legendFormat" => $query[$name_of_query]['label'], "expr" => $name_of_query }]
+            } else {
+                $accu + [{ "expr" => "${query}" }]
+            }
+        }
+
+        $memo + [{ "id" => undef, "datasource" => $datasource, "targets" => $queries, "title" => $nameOfPanel, "type" => $type, "legend" => $legend }]
    }   
      $data_for_api = {
                       "dashboard" => {
