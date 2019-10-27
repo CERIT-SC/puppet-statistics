ee9e34ae96b3d13374ec631e6d4bb3d2123a9548
marek130 <marekpastierik15@gmail.com>
2019-10-27 12:41:27 +0100

    typo

--- manifests/probes/collectd/plugin.pp
+++ manifests/probes/collectd/plugin.pp
@@ -4,7 +4,7 @@ define statistics::probes::collectd::plugin (
    Integer $interval = 300,
    Boolean $empty    = true,
 ) {
-   file { "${statistics::path_to_plugins}/${title}.conf":
+   file { "${statistics::collectd_path_to_plugins}/${title}.conf":
      ensure  => 'present',
      content => epp('statistics/plugin_collectd.epp', { "type" => $type, "options" => $settings, "interval" => $interval }),
      require => Package[$::statistics::type_of_probs],
