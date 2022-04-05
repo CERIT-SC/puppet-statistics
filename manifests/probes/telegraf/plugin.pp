b6f74001ab36a8ec8511a99de9c9e2af0565a7fa
Lukas Hejtmanek <xhejtman@gmail.com>
2022-04-05 09:33:24 +0200

    MANIFESTS: fix file mode

--- manifests/probes/telegraf/plugin.pp
+++ manifests/probes/telegraf/plugin.pp
@@ -5,7 +5,8 @@ define statistics::probes::telegraf::plugin (
 ) {
     file { "${::statistics::telegraf_path_to_plugins}/${title}.conf":
         ensure  => "present",
-        mode    => "0644",
+        mode    => "0600",
+        owner   => 'telegraf',
         content => epp('statistics/plugin_telegraf.epp', { "type" => $type, "options" => $settings }),
         require => Package[$::statistics::type_of_probes],
         notify  => Service['telegraf'],
